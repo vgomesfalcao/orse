af57393410734ae0989debd34a730a90f177bf6c
Vinicius <vgomesfalcao@gmail.com>
2022-09-01 14:21:27 -0300

    New files

---
+++ scripts/importfiles.sh
@@ -0,0 +1,18 @@
+
+from-stage(){
+    if [[ -z $1 ]]
+    then
+        rsync -avhP ${STAGING_SSH_USERNAME}@${STAGING_SSH_HOSTNAME}:${STAGING_PROJECT_LOCATION}/orse/orse_d/web/sites/default/files/ ./web/sites/default/files
+        chmod -R 775 ./web
+        chown -R www-data:www-data ./web
+        return 0
+    fi
+    return 1
+}
+
+
+set -o allexport
+source .env
+set +o allexport
+
+$1 $2
