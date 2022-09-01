0dba9a5621b9e10c14a47c2613ce67d3eeb17b44
Vinicius <vgomesfalcao@gmail.com>
2022-09-01 14:20:51 -0300

    Theme ajust

--- web/themes/custom/orse_eventos/webpack.mix.js
+++ web/themes/custom/orse_eventos/webpack.mix.js
@@ -7,7 +7,7 @@
  | for your application. See https://github.com/JeffreyWay/laravel-mix.
  |
  */
-const proxy = 'http://localhost/orse_d/web/';
+const proxy = 'https://orse_d.lndo.site/';
 const mix = require('laravel-mix');
 
 /*
