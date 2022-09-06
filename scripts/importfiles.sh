e85a8e1e687bfbef80dd26e23f8d9f4e7fb8435c
Vinicius Gomes <vgomesfalcao@gmail.com>
2022-09-06 13:34:10 -0300

    Create Paragraph Banner with link

--- scripts/importfiles.sh
+++ scripts/importfiles.sh
@@ -2,9 +2,9 @@
 from-stage(){
     if [[ -z $1 ]]
     then
-        rsync -avhP ${STAGING_SSH_USERNAME}@${STAGING_SSH_HOSTNAME}:${STAGING_PROJECT_LOCATION}/orse/orse_d/web/sites/default/files/ ./web/sites/default/files
-        chmod -R 775 ./web
-        chown -R www-data:www-data ./web
+        rsync -avhP ${STAGING_SSH_USERNAME}@${STAGING_SSH_HOSTNAME}:${STAGING_PROJECT_LOCATION}/orse/orse_d/web/sites/default/files/ ../web/sites/default/files
+        sudo chmod -R 775 ../web
+        sudo chown -R www-data:www-data ../web
         return 0
     fi
     return 1
@@ -12,7 +12,7 @@ from-stage(){
 
 
 set -o allexport
-source .env
+source ../.env
 set +o allexport
 
 $1 $2
